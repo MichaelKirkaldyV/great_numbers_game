0532c515939b1be17a4b5005f0985c98df931798
Michael <vmichaelkirkaldy@live.com>
2018-06-11 11:44:54 -0500

    updates to server.py

--- server.py
+++ server.py
@@ -8,10 +8,15 @@ app.secret_key = 'super secret key'
 def index():
 	return render_template('index.html')
 
+#methods use post, only for forms. A normal, URL or any route is GET by default.
 @app.route('/guess', methods=['POST'])
 def guess_number():
+	#sets the num variable of the form submitted to guess
 	guess = request.form['number']
+	#always use .get when getting session variables
+	#sess[] is used to set variables 
 	if session.get('number') is None:
+		#sets random range to session
        	 session['number'] = random.randrange(0, 101)
 	if session.get('number') == guess:
 		 return render_template('answer.html')
@@ -20,8 +25,9 @@ def guess_number():
 	else:
 		 return render_template('too_high.html')
 
-@app.route('/reset', methods=['POST'])
+@app.route('/reset', methods=['GET'])
 def reset():
+	#deletes the session, and returns to homepage where variable is set to None.
 	session.pop('number')
 	return redirect('/')
 
